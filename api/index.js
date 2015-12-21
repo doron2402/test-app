6fdede880d636c6e16df5d69ac75a250bf07ab91
Doron Segal <doron2402@gmail.com>
2015-12-21 10:12:11 -0800

    working on an api

--- api/index.js
+++ api/index.js
@@ -1,23 +1,17 @@
 var settings = require('./settings');
 var express = require('express');
-var ejs =  require('ejs');
 var app = express();
 var routes = require('./routes');
 var mw = require('./mw');
-
-app.engine('.html',ejs.__express);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
 
 app
   .use('/', routes.base)
   .use('/users', routes.users)
   .use(mw.errorHandler)
-  .use('/public', express.static(__dirname + '/public'));
 
 app.listen(settings.server.port, function(err){
   if (err){
     console.error(err);
   }
-  console.log('Hi Server is running listen on port: ' + settings.server.port);
+  console.log('API is running listen on port: ' + settings.server.port);
 });
